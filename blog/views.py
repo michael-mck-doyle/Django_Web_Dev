887f9300613e95ef8ac38e080e39d7fa7109d306
Michael Doyle <meyekkul@gmail.com>
2021-02-04 15:10:19 +0800

    add blog post to db

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 # Create your views here.
 def index(request):
-
-    return render(request, 'blog/index.html')
+    posts = Post.objects.all()
+    context = {'posts' : posts}
+    return render(request, 'blog/index.html', context=context)
